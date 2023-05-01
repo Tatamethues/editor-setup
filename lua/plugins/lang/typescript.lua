dec86e6fb8a131306fdcee929f03f7ca65db08a3
Colin Shen <colinshen.work@outlook.com>
2023-05-01 23:13:51 +0800

    ts

---
+++ lua/plugins/lang/typescript.lua
@@ -0,0 +1,26 @@
+return {
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     if type(opts.ensure_installed) == "table" then
+  --       vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "williamboman/mason-lspconfig.nvim",
+  --   opts = function(_, opts)
+  --     if type(opts.ensure_installed) == "table" then
+  --       vim.list_extend(opts.ensure_installed, { "rome" })
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       rome = {},
+  --     },
+  --   },
+  -- },
+}
