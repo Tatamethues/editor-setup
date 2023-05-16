4443cc817a54e90bc0285da7a3fac1cadd43bf6c
Colin Shen <colinshen.work@outlook.com>
2023-05-16 17:52:27 +0800

    add rust

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -9,7 +9,15 @@ return {
     end,
   },
   {
-    "simart39/rust-tools.nvim",
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "rust-analyzer" })
+      end
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
     ft = { "rust" },
   },
 }
