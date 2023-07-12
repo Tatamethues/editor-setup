441c7d0b4b22199f82f9eec66381ccfa7af7e0cf
Colin Shen <colinshen.work@outlook.com>
2023-07-12 09:13:57 +0800

    using lazyvim rust config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,13 +13,14 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
     { import = "plugins/lang/typescript" },
-    { import = "plugins/lang/rust" },
+    -- { import = "plugins/lang/rust" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
