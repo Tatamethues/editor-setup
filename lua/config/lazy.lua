1fdbf2df1ae6d2c22bf5fceddb62d733c3dcec68
Colin Shen <colinshen.work@outlook.com>
2023-07-12 23:01:13 +0800

    u

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
     { import = "plugins/lang/typescript" },
-    -- { import = "plugins/lang/rust" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
